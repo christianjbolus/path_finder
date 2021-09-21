5fd3b46988298cab4fdbe77a864fb5a33c044f3e
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-09-21 16:00:09 -0400

    Remove boilerplate from App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Grid from './components/Grid/Grid';
 
 function App() {
   return (
     <div className="App">
       <h1>Path Finder</h1>
+      <Grid />
     </div>
   );
 }
