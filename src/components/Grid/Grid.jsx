ea480fc8ee253dc1e8db31fa64a82ab721f74c8c
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-09-21 18:09:38 -0400

    Initialize inital state to represent grid

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import GridSquare from '../GridSquare/GridSquare'
+import { useState } from 'react';
+import { generateGrid } from '../../utils/helpers';
+import GridSquare from '../GridSquare/GridSquare';
+import './Grid.css'
 
 export default function Grid() {
+  const [rows, setRows] = useState(25);
+  const [cols, setCols] = useState(25);
+  const [grid, setGrid] = useState(generateGrid(rows, cols, 0));
+
   return (
     <div className="grid">
-      <GridSquare />
+      {grid.map((row, i) => row.map((square, j) => <GridSquare i={i} j={j} />))}
     </div>
-  )
+  );
 }
