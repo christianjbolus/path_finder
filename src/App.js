ecb719b562ebd71d51c1b12b2040f9c90669eafb
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-09-30 09:52:46 -0400

    Update styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Grid from './components/Grid/Grid';
+import Button from './components/Button/Button'
 
 function App() {
   return (
     <div className="App">
       <h1>Path Finder</h1>
       <Grid />
+      <Button text="Execute" className="btn"/>
     </div>
   );
 }
