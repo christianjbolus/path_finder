354d8917cf27fffbfb254d7f02209ecf03cea236
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-01 15:59:25 -0400

    Style Grid component using css-in-js

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,7 +1,15 @@
+/** @jsxImportSource @emotion/react */
 import { useState } from 'react';
 import { generateGrid } from '../../utils/helpers';
 import GridSquare from '../GridSquare/GridSquare';
-import './Grid.css'
+import { css } from '@emotion/react';
+
+const gridStyle = css`
+  display: inline-grid;
+  grid-template-columns: repeat(25, 1fr);
+  grid-template-rows: repeat(25, 1fr);
+  margin-bottom: 40px;
+`
 
 export default function Grid() {
   const [rows, setRows] = useState(25);
@@ -9,7 +17,7 @@ export default function Grid() {
   const [grid, setGrid] = useState(generateGrid(rows, cols, 0));
 
   return (
-    <div className="grid">
+    <div css={gridStyle}>
       {grid.map((row, i) => row.map((square, j) => <GridSquare i={i} j={j} key={`${i}:${j}`}/>))}
     </div>
   );
